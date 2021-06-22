c13d0e5fb382ba349193a3058e29688fb8117a0f
chrwoizi <christian@woizischke.de>
2021-06-22 20:01:58 +0200

    filter calendar events

--- server/src/environments/environment.shared.ts
+++ server/src/environments/environment.shared.ts
@@ -1,7 +1,12 @@
+import { CalendarEvent } from '../../../shared/CalendarEvent';
+
 export const sharedEnvironment = {
   locationLatitude: 52.477093,
   locationLongitude: 10.090432,
 
   locale: 'de',
   timezone: 'Europe/Berlin',
+
+  filterCalendarEvent: (event: CalendarEvent) =>
+    /woiziframe\s*:\s*nein/.test(event.description?.toLowerCase()),
 };
