217a0ca968a11de83425c35453849f971f2f6df1
chrwoizi <christian@woizischke.de>
2021-09-28 11:12:54 +0200

    add screen switcher

---
+++ piscreen/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { Environment } from './environment.type';
+
+export const environment: Environment = {
+  production: true,
+  host: '0.0.0.0',
+  port: 4202,
+};
---
+++ piscreen/src/environments/environment.type.ts
@@ -0,0 +1,5 @@
+export interface Environment {
+  production?: boolean;
+  host?: string;
+  port?: number;
+}
